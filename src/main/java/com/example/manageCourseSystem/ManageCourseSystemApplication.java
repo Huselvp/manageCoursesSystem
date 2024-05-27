6bc154c97f5e1d3a851ec2019ae277906f51f209
Huselvp <houdamesbahi51@gmail.com>
2024-05-27 10:51:48 +0100

    first commit

---
+++ src/main/java/com/example/manageCourseSystem/ManageCourseSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.manageCourseSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageCourseSystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ManageCourseSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/manageCourseSystem/ManageCourseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manageCourseSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageCourseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
