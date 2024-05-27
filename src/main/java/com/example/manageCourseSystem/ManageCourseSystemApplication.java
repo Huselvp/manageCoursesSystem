02e9b9133e2ec0c294302be7fbd8c1b141cbec71
Huselvp <houdamesbahi51@gmail.com>
2024-05-27 11:00:38 +0100

    second commit

--- src/main/java/com/example/manageCourseSystem/ManageCourseSystemApplication.java
+++ src/main/java/com/example/manageCourseSystem/ManageCourseSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ManageCourseSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ManageCourseSystemApplication.class, args);
+		System.out.println("nothing");
 	}
 
 }
